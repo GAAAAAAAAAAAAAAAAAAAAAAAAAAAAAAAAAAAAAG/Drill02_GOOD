dec33295910dee031809c4ac114079a9eabb971b
GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAG <seogaeun@tukorea.ac.kr>
2023-09-20 17:57:56 +0900

    원그리기 완성

---
+++ character_moves.py
@@ -0,0 +1,38 @@
+import math
+from pico2d import *
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+x = 0
+
+# 여기를 채우세요.
+def run_circle():
+    print('CIRCLE')
+   
+
+    cx,cy,r = 400,300,200
+    for deg in range(0,360,5):
+        x = cx + r * math.cos(deg/360*2*math.pi)
+        y = cy + r * math.sin(deg/360*2*math.pi)
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        delay(0.01)
+        
+    
+    pass
+
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+
+while True:
+    run_circle()
+    run_rectangle()
+    break
+
+close_canvas()
+
