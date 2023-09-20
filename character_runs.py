52e6e3e04444cac2e89ad3d92b402a805c5a3911
GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAG <seogaeun@tukorea.ac.kr>
2023-09-20 17:43:37 +0900

    rectangle, circle 뼈대 완성

--- character_runs.py
+++ character_runs.py
@@ -1,12 +1,26 @@
+import math
 from pico2d import *
+
 open_canvas()
+
 grass = load_image('grass.png')
 character = load_image('run_animation.png')
 
 x = 0
 
 # 여기를 채우세요.
+def run_circle():
+    print('CIRCLE')
+    pass
 
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+
+while True:
+    run_circle()
+    run_rectangle()
+    break
 
 close_canvas()
 
